84299ae3946872e4ad1efbb696ac26d7ebd6f807
Eloi RABEC <eloi.rabec@gmail.com>
2022-01-07 09:47:51 +0100

    <commit>Projet avec pb de lancement

---
+++ TestAppMobile3/TestAppMobile3/AppShell.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace TestAppMobile3
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
